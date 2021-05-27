f2fff6426a5662cd4d48baf4c527bd370a27d2c3
Jianing Wang <79324726+24wangj@users.noreply.github.com>
2021-05-27 19:23:06 -0400

    Add files via upload

---
+++ Solution001.java
@@ -0,0 +1,17 @@
+
+public class Solution001 {
+	
+	public static void main(String[] args) {
+		
+		int totalSum = 0;
+		
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				totalSum += i;
+			}
+		}
+		
+		System.out.println(totalSum);
+		
+	}
+}
