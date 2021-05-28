0d43a0d2d7900ec173058a9ac0fa1d040f179122
Jianing Wang <79324726+24wangj@users.noreply.github.com>
2021-05-28 00:15:54 -0400

    Add files via upload

---
+++ java/Solution006.java
@@ -0,0 +1,27 @@
+
+public class Solution006 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int squareNum = 0;
+		int squareSum = 0;
+		int result;
+		
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		
+		sum = sum * sum;
+		
+		for (int k = 1; k <= 100; k++) {
+			squareNum = k * k;
+			squareSum += squareNum;
+		}
+		
+		result = sum - squareSum;
+
+		System.out.println(result);
+		
+	}
+}
