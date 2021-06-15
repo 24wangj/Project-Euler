606c6eb12788cb478e4a6db4b1516ddd3660e64e
Jianing Wang <79324726+24wangj@users.noreply.github.com>
2021-06-15 18:48:00 -0400

    Update Solution005.java

--- java/Solution005.java
+++ java/Solution005.java
@@ -6,18 +6,8 @@ public class Solution005 {
 		int num = 0;
 		boolean isMultiple = false;
 		
-		//This is a dumb solution
 		for (int i = 0; isMultiple != true; i++) {
 			if (i > 0 &&
-				i % 2 == 0 &&
-				i % 3 == 0 &&
-				i % 4 == 0 &&
-				i % 5 == 0 &&
-				i % 6 == 0 &&
-				i % 7 == 0 &&
-				i % 8 == 0 &&
-				i % 9 == 0 &&
-				i % 10 == 0 &&
 				i % 11 == 0 &&
 				i % 12 == 0 &&
 				i % 13 == 0 &&
