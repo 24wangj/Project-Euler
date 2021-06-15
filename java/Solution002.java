4ead7ef68dc6436e491f40a0c64e64d9f85c6089
Jianing Wang <79324726+24wangj@users.noreply.github.com>
2021-06-15 18:46:15 -0400

    Update Solution002.java

--- java/Solution002.java
+++ java/Solution002.java
@@ -3,7 +3,10 @@ public class Solution002 {
 
 	public static void main(String[] args) {
 		
-		int num1 = 0, num2 = 1, sum = 0, result = 0;
+		int num1 = 0;
+		int num2 = 1;
+		int sum = 0;
+		int result = 0;
 		
 		while (sum <= 4000000) {
 			sum = num1 + num2;
